1117c61f596193e71852e172b1a4a41f80fb6ae2
Kaka <kaka.xie@mrobot.cn>
2019-02-27 09:43:33 +0800

    add photoelectric switch state filter

--- Modules/photoelectric_switch.h
+++ Modules/photoelectric_switch.h
@@ -8,6 +8,8 @@
 #define PHO_SWITCH_2_TRIGGERED      0x02
 #define PHO_SWITCH_3_TRIGGERED      0x04
 
+#define PHO_SWITCH_NUM_MAX          3
+
 
 extern uint8_t pho_switch_state;
 
--- Task/conveyor_belt_task.c
+++ Task/conveyor_belt_task.c
@@ -8,6 +8,7 @@
 #include "platform.h"
 #include "photoelectric_switch.h"
 #include "conveyor_belt.h"
+#include <string.h>
 
 OS_STK conveyor_belt_task_stk[CONVEYOR_BELT_TASK_STK_SIZE] = {0};
 OS_STK pho_switch_status_task_stk[PHO_SWITCH_STATUS_TASK_STK_SIZE] = {0};
@@ -15,15 +16,71 @@ OS_STK pho_switch_status_task_stk[PHO_SWITCH_STATUS_TASK_STK_SIZE] = {0};
 extern void upload_conveyor_belt_status(uint8_t status);
 
 
+#define PHO_SWITCH_STATE_BUF_SIZE   3
 void pho_switch_status_task(void *pdata)
 {
+    uint8_t state_buf[PHO_SWITCH_STATE_BUF_SIZE] = {0};
+    uint8_t state_cnt_buf[PHO_SWITCH_NUM_MAX] = {0};
+    uint8_t state_tmp = 0;
+    uint8_t cnt = 0;
+    uint8_t i = 0;
     delay_ms(5000);
     while(1)
     {
+        state_buf[cnt] = get_pho_switch_state();
+        if(cnt < PHO_SWITCH_STATE_BUF_SIZE)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt = 0;
+        }
+        memset(state_cnt_buf, 0, sizeof(state_cnt_buf));
+        for(i = 0; i < PHO_SWITCH_STATE_BUF_SIZE; i++)
+        {
+            if(state_buf[i] & PHO_SWITCH_1_TRIGGERED)
+            {
+                state_cnt_buf[0]++;
+            }
+            if(state_buf[i] & PHO_SWITCH_2_TRIGGERED)
+            {
+                state_cnt_buf[1]++;
+            }
+            if(state_buf[i] & PHO_SWITCH_3_TRIGGERED)
+            {
+                state_cnt_buf[2]++;
+            }
+        }
+        if(state_cnt_buf[0] > PHO_SWITCH_STATE_BUF_SIZE / 2)
+        {
+            state_tmp |= PHO_SWITCH_1_TRIGGERED;
+        }
+        else
+        {
+            state_tmp &= ~PHO_SWITCH_1_TRIGGERED;
+        }
+        if(state_cnt_buf[1] > PHO_SWITCH_STATE_BUF_SIZE / 2)
+        {
+            state_tmp |= PHO_SWITCH_2_TRIGGERED;
+        }
+        else
+        {
+            state_tmp &= ~PHO_SWITCH_2_TRIGGERED;
+        }
+        if(state_cnt_buf[2] > PHO_SWITCH_STATE_BUF_SIZE / 2)
+        {
+            state_tmp |= PHO_SWITCH_3_TRIGGERED;
+        }
+        else
+        {
+            state_tmp &= ~PHO_SWITCH_3_TRIGGERED;
+        }
         OS_ENTER_CRITICAL();
-        pho_switch_state = get_pho_switch_state();
+        //pho_switch_state = get_pho_switch_state();
+        pho_switch_state = state_tmp;
         OS_EXIT_CRITICAL();
-        delay_ms(50);
+        delay_ms(20);
     }
 }
 
@@ -92,12 +149,8 @@ void conveyor_belt_task(void *pdata)
                             conveyor_belt.work_mode = CONVEYOR_BELT_STATUS_STOP;
                             OS_EXIT_CRITICAL();
                             work_mode = CONVEYOR_BELT_STATUS_STOP;
-                            load_state = 1;
+                            upload_conveyor_belt_status(CONVEYOR_LOAD_FINISHED);
                         }
-                        break;
-
-                    case 1:
-                        load_state = 0;
                         break;
                     default :
                         break;
@@ -132,6 +185,7 @@ void conveyor_belt_task(void *pdata)
                             OS_EXIT_CRITICAL();
                             work_mode = CONVEYOR_BELT_STATUS_STOP;
                             unload_state = 0;
+                            upload_conveyor_belt_status(CONVEYOR_UNLOAD_FINISHED);
                         }
                         break;
 
